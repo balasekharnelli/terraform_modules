826d33b608d10445921a6f26455fa791b7b9881f
balasekharnelli <nelli.sekhar89@gmail.com>
2019-07-24 10:04:19 +0530

    Removed secret keys

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIA3NLEUHAHDSGHDGECG4AXPFHX689382y48923y4934y8"
-  secret_key = "WvgQdvaBwLEMs64iw0nkfTNALd/+qB9ZDIjWImCJ434345u4yu54y3iuy"
+  access_key = ""
+  secret_key = ""
   region     = "us-east-1"
 }
